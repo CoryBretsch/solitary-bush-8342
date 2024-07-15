0f2df7c58c75eb590e06d43f949980b80ae333be
Cory Bretsch <corybretsch@gmail.com>
2024-07-15 17:35:49 -0500

    user story 2... after i took dog for a walk

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/ingredients", to: "ingredients#index"
+  get "/recipes/:id", to: "recipes#show"
 
 
 
---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "the recipes show page" do
+  it "should display recipe's name, complexity, genre, and list ingredient names" do
+    
+    oil = Ingredient.create!(name: "oil", cost: 2)
+    garlic = Ingredient.create!(name: "garlic", cost: 1)
+    garlic_oil = Recipe.create(name: "garlic oil", complexity: 5, genre: "dip")
+    RecipeIngredient.create!(recipe: garlic_oil, ingredient: oil)
+    RecipeIngredient.create!(recipe: garlic_oil, ingredient: garlic)
+
+    visit "/recipes/#{garlic_oil.id}"
+
+    expect(page).to have_content(garlic_oil.name)
+    expect(page).to have_content(garlic_oil.complexity)
+    expect(page).to have_content(garlic_oil.genre)
+    expect(page).to have_content(garlic.name)
+    expect(page).to have_content(oil.name)
+  end
+end
